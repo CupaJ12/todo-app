ff0da6dca0763f04f197ced6f5d16fa432fd950f
CupaJ12 <108900676+CupaJ12@users.noreply.github.com>
2022-12-20 11:33:19 -0500

    huge progress on client stuff

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,24 +7,41 @@ function onReady() {
 console.log('in onready');
 
 
-//  $('#submitTask').on('click', submitTaskFunction);
+ $('#submitTask').on('click', collectTaskFunction);
+}
+
+///////////////////clientSide functions///////////////////
+let task = ''
+function collectTaskFunction() {
+console.log('in submitTaskFunction');
+task = $('#userTask').val();
+console.log('taskName: ', task);
 }
 
 
+/////////////////// server/client interactions ///////////////////
 
+function postTaskFunction (){
+    console.log('in postTaskFunction');
+    $ajax({
+        type: 'POST',
+        url: '/tasks',
+        data: $('#userTask').val(),
+    }).then (function(response){
+        console.log('post response from server', response);
+        getTasks();
+    }).catch (function(error) {
+        console.log('error:', error);
+        alert(error.responseText);
+    });
+}
 
-
-
-
-///////////////////submit task function server stuff///////////////////
-
-// function submitTaskFunction(){
-//     console.log('submitTaskFunction');
-//     // var taskName = $('#userTask').val();
-//     console.log('userTask:'+ userTask);
-//     $.ajax({
-//         type: 'POST',
-//         url: '/tasks',
-//         data: 'userTask',
-// });
-// }
+function getTasks() {
+    console.log('in getTasks');
+    $ajax({
+        type: 'GET',
+        url: '/tasks',
+}).then (function(response){
+    console.log('get tasks response from server', response);
+    tasksToDOM(response);
+})}
